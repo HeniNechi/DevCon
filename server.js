e8dee7cb2a677b793ee59645eb1175cc21ec1ab0
HeniNechi <hani.nechi@yahoo.fr>
2019-10-30 18:24:36 +0100

    Adding passport

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport")
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
+
+
+
+
+
 const app = express();
+
+//  Body parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -13,16 +24,20 @@ const db = require("./config/keys").mongoURI;
 // Connect to Mongo DB
 
 mongoose
-.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err))
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// Passport middleware 
+app.use(passport.initialize());
 
-// use Routes 
-app.use('/api/users',users)
-app.use('/api/profile',profile)
-app.use('/api/posts',posts)
+//Passport Config
+require('./config/passport')(passport)
+
+// use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
